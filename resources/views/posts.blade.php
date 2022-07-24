7d48097e2d6a30fea44144d73232c63378e85c0d
Jake Bogan <jakebogan01@gmail.com>
2022-07-24 17:11:33 -0400

    working on gathering all blogs in file system

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,27 +4,14 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+    @foreach($posts as $post)
+        <article>
+{{--            <h1><a href="/posts/my-first-post">My First Post</a></h1>--}}
 
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut commodi consequatur culpa delectus et eveniet ex illum, iure minima quaerat, quis unde voluptatem? Architecto autem dolores ducimus, eos esse ex fuga harum id laboriosam molestias nam nesciunt quod saepe suscipit, vero! Adipisci aliquid amet autem consequatur dolores excepturi expedita explicabo facere facilis illum, ipsa labore obcaecati perspiciatis quis quos repellendus saepe similique. Accusantium adipisci alias aspernatur aut cumque debitis dicta dolor doloremque eius eligendi et expedita fugiat ipsam laboriosam laudantium libero maxime minima nisi nobis nostrum officiis omnis provident quas quibusdam quo repellat sed tempore, totam unde ut! Blanditiis, placeat.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut commodi consequatur culpa delectus et eveniet ex illum, iure minima quaerat, quis unde voluptatem? Architecto autem dolores ducimus, eos esse ex fuga harum id laboriosam molestias nam nesciunt quod saepe suscipit, vero! Adipisci aliquid amet autem consequatur dolores excepturi expedita explicabo facere facilis illum, ipsa labore obcaecati perspiciatis quis quos repellendus saepe similique. Accusantium adipisci alias aspernatur aut cumque debitis dicta dolor doloremque eius eligendi et expedita fugiat ipsam laboriosam laudantium libero maxime minima nisi nobis nostrum officiis omnis provident quas quibusdam quo repellat sed tempore, totam unde ut! Blanditiis, placeat.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut commodi consequatur culpa delectus et eveniet ex illum, iure minima quaerat, quis unde voluptatem? Architecto autem dolores ducimus, eos esse ex fuga harum id laboriosam molestias nam nesciunt quod saepe suscipit, vero! Adipisci aliquid amet autem consequatur dolores excepturi expedita explicabo facere facilis illum, ipsa labore obcaecati perspiciatis quis quos repellendus saepe similique. Accusantium adipisci alias aspernatur aut cumque debitis dicta dolor doloremque eius eligendi et expedita fugiat ipsam laboriosam laudantium libero maxime minima nisi nobis nostrum officiis omnis provident quas quibusdam quo repellat sed tempore, totam unde ut! Blanditiis, placeat.
-        </p>
-    </article>
+{{--            <p>--}}
+{{--                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut commodi consequatur culpa delectus et eveniet ex illum, iure minima quaerat, quis unde voluptatem? Architecto autem dolores ducimus, eos esse ex fuga harum id laboriosam molestias nam nesciunt quod saepe suscipit, vero! Adipisci aliquid amet autem consequatur dolores excepturi expedita explicabo facere facilis illum, ipsa labore obcaecati perspiciatis quis quos repellendus saepe similique. Accusantium adipisci alias aspernatur aut cumque debitis dicta dolor doloremque eius eligendi et expedita fugiat ipsam laboriosam laudantium libero maxime minima nisi nobis nostrum officiis omnis provident quas quibusdam quo repellat sed tempore, totam unde ut! Blanditiis, placeat.--}}
+{{--            </p>--}}
+            {{ $post }}
+        </article>
+    @endforeach
 </body>
